1e1a7faaa1e3e282b7dd550cb2460d67b2506d45
Simon Onyszczak Hansen <simo401t@zbc.dk>
2024-09-16 14:26:06 +0200

    Updated CSS and HTML files in various components, including about-page, footer, homepage, navbar, and projects-page, with changes to styles, layouts, and content.

--- src/app/projects-page/projects-page.component.ts
+++ src/app/projects-page/projects-page.component.ts
@@ -1,16 +1,28 @@
-import { Component, ElementRef, ViewChildren, AfterViewInit, QueryList } from '@angular/core';
+import { Component, ElementRef, ViewChildren, AfterViewInit, QueryList, OnInit, OnDestroy } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-projects-page',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, NgFor],
   templateUrl: './projects-page.component.html',
   styleUrls: ['./projects-page.component.css']
 })
-export class ProjectsPageComponent implements AfterViewInit {
+export class ProjectsPageComponent implements AfterViewInit, OnInit, OnDestroy {
   @ViewChildren('projectCard') projectCards!: QueryList<ElementRef>;
+
+  images: string[] = [
+    "assets/images/PasswordGenerator.png",
+    "assets/images/PasswordGenerator2.png"
+  ];
+  currentSlide: number = 0;
+  slideIntervalId: any;
+
+  ngOnInit(): void {
+    this.startSlideshow();
+  }
 
   ngAfterViewInit() {
     const observer = new IntersectionObserver((entries, observer) => {
@@ -27,5 +39,22 @@ export class ProjectsPageComponent implements AfterViewInit {
     this.projectCards.forEach((projectCard) => {
       observer.observe(projectCard.nativeElement);
     });
+  }
+
+  ngOnDestroy(): void {
+    this.clearSlideshow();
+  }
+
+  // Slideshow logic
+  startSlideshow() {
+    this.slideIntervalId = setInterval(() => {
+      this.currentSlide = (this.currentSlide + 1) % this.images.length;
+    }, 8000);
+  }
+
+  clearSlideshow() {
+    if (this.slideIntervalId) {
+      clearInterval(this.slideIntervalId);
+    }
   }
 }

250e9145887e5eda7f7a7d931d012c690eb8b91a
Simon Onyszczak Hansen <simo401t@zbc.dk>
2025-01-20 13:55:13 +0100

    Removed FooterComponent import from AppComponent, since it was not used

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet, Route, Router } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from "./footer/footer.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

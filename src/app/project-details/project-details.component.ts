bf349b10b4fcbe199d8a63b7f8238b04f7d9557e
Simon Onyszczak Hansen <simo401t@o365.celf.dk>
2025-01-22 12:22:59 +0100

    Added a working button for going back when the user routes to a project details page

--- src/app/project-details/project-details.component.ts
+++ src/app/project-details/project-details.component.ts
@@ -1,21 +1,42 @@
+import { NgIf, Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-project-details',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './project-details.component.html',
   styleUrl: './project-details.component.css'
 })
-export class ProjectDetailsComponent implements OnInit{
+export class ProjectDetailsComponent implements OnInit {
   projectId!: string;
+  loading: boolean = true;
+  error: string | null = null;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private location: Location) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
-      this.projectId = params.get('id') || '';
-    })
+      const id = params.get('id');
+      if (id) {
+        this.projectId = id;
+        this.fetchProjectDetails();
+      } else {
+        this.error = 'Project ID not found';
+      }
+      this.loading = false;
+    }, error => {
+      this.error = 'Failed to load project details';
+      this.loading = false;
+    });
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  fetchProjectDetails(): void {
+
   }
 }
